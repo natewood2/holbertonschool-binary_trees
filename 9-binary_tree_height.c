4c4139f6c15be019a1c606e2d0ae5586f548e439
natewood2 <nathanwood.sfg@gmail.com>
2023-12-20 14:32:57 -0600

    maybe

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,7 +8,8 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height = 0;
+	size_t left_height = 0;
+    size_t right_height = 0;
 
 	if (tree == NULL)
 	{
@@ -24,8 +25,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		right_height = 1 + binary_tree_height(tree->right);
 	}
 	if (left_height > right_height)
-	{
 		return (left_height);
-	}
-	return (right_height);
+    else
+	    return (right_height);
 }
