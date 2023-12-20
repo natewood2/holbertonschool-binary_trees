cf25b85ba898d2df5c7fccdca7c7198b5f762a78
natewood2 <nathanwood.sfg@gmail.com>
2023-12-20 10:53:32 -0600

    added node to the left of the tree

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - a function that inserts a node as the left of another node
+ * @parent: parent node
+ * @value: value of the node
+ * Return: new node
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    if (parent == NULL) 
+    {
+        return NULL;
+    }
+
+    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+    if (new_node == NULL) 
+    {
+        return (NULL);
+    }
+
+    new_node->n = value;
+    new_node->parent = parent;
+    new_node->left = parent->left;
+    new_node->right = NULL;
+
+    if (parent->left != NULL) 
+    {
+        parent->left->parent = new_node;
+    }
+    parent->left = new_node;
+
+    return (new_node);
+}
