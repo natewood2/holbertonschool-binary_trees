c6d4b2c68b772ff6c638649ffdc6ba8bd7f67316
natewood2 <nathanwood.sfg@gmail.com>
2023-12-20 14:29:33 -0600

    tree heights

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: awesome little trees
+ * Return: multiple things depending on stuff or just a 0
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left != NULL)
+	{
+		left_height = 1 + binary_tree_height(tree->left);
+	}
+	if (tree->right != NULL)
+	{
+		right_height = 1 + binary_tree_height(tree->right);
+	}
+	if (left_height > right_height)
+	{
+		return (left_height);
+	}
+	return (right_height);
+}
