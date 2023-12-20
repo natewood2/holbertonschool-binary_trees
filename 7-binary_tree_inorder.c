f6b23680fc9a123775a4fa3e0c34453b6c263dab
natewood2 <nathanwood.sfg@gmail.com>
2023-12-20 14:04:44 -0600

    fixed white space

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - goes through a binary tree using in-order traversal 
+ * binary_tree_inorder - goes through a binary tree using in-order traversal
  * @tree: the tree
  * @func: func a func a func a func
 */
