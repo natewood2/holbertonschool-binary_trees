4786a67f51775c9d21c8b98c70c8387e4510f1a9
natewood2 <nathanwood.sfg@gmail.com>
2023-12-20 11:25:39 -0600

    Check is we gots a leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: node which is leaf or not hehe
+ * Return: 0 indicating not leaf or 1 which is leaf
+*/
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (node == NULL)
+    {
+        return (0);
+    }
+
+    if (node->right == NULL && node->left == NULL)
+    {
+        return (1);
+    }
+    return (0);
+}
